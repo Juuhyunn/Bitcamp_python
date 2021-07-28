ce3c24f835e68f60bcb4b19bf7d8fa244050edc8
Juuhyunn <ahnju96@gmail.com>
2021-07-28 18:58:04 +0900

    28, July

--- modu/template/pop_structure_visual_with_pandas.py
+++ modu/template/pop_structure_visual_with_pandas.py
@@ -99,9 +99,9 @@ if __name__ == '__main__':
     pop = Population()
     # pop.show_plot(pop.pop_per_dong('방이1동'))
     # pop.pop_per_dong('필동')
-
+    name = input('지역?')
     pop.read_data()
-    pop.find_home('신림동')
+    pop.find_home(name)
     pop.home = pop.list_to_array(pop.home)
-    pop.find_similar_area('필동')
-    pop.show_plot_similar_two_cities('필동', pop.home, pop.away)
+    pop.find_similar_area(name)
+    pop.show_plot_similar_two_cities(name, pop.home, pop.away)
